f8827daeee51f6946892f97fc01f8714723ba149
stpafk <stephan.weigert@hotmail.com>
2023-11-03 17:08:40 -0300

    start router and controllers

---
+++ routes/catalog.js
@@ -0,0 +1,8 @@
+const express = require("express");
+const router = express.Router();
+
+const artist_controller = require("../controllers/artistControllers");
+const vinyl_controller = require("../controllers/vinylController");
+const vinylInstance_controller = require("../controllers/vinylInstanceController");
+const genre_controller = require("../controllers/genreController");
+
