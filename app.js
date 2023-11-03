1c2fb082664c5cbd05564d7e1d28b9156c63e269
stpafk <stephan.weigert@hotmail.com>
2023-11-03 17:40:20 -0300

    add catalog router

--- app.js
+++ app.js
@@ -3,11 +3,12 @@ var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
-const mongoose = require('mongoose')
+const mongoose = require('mongoose');
 require("dotenv").config();
 
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
+const catalogRouter =  require('./routes/catalog');
 
 var app = express();
 
@@ -18,7 +19,7 @@ main().catch((err) => console.log(err));
 async function main() {
   await mongoose.connect(mongoDB);
 }
-
+//
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'jade');
@@ -29,8 +30,10 @@ app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
+
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
+app.use('/catalog', catalogRouter);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
--- routes/catalog.js
+++ routes/catalog.js
@@ -6,3 +6,42 @@ const vinyl_controller = require("../controllers/vinylController");
 const vinylInstance_controller = require("../controllers/vinylInstanceController");
 const genre_controller = require("../controllers/genreController");
 
+//artist routes
+router.get("/artist/create", artist_controller.artist_create_get);
+router.post("artist/create", artist_controller.artist_create_post);
+router.get("/artist/:id/delete", artist_controller.artist_delete_get);
+router.post("/artist/:id/delete", artist_controller.artist_delete_post);
+router.get("/artist/:id/update", artist_controller.artist_update_get);
+router.post("/artist/:id/update", artist_controller.artist_update_post);
+router.get("/artist/:id", artist_controller.artist_detail);
+router.get("/artist", artist_controller.artist_list);
+
+//vynil routes
+router.get("/vinyl/create", vinyl_controller.vinyl_create_get);
+router.post("/vynil/create", vinyl_controller.vinyl_create_post);
+router.get("/vinyl/:id/delete", vinyl_controller.vinyl_delete_get); 
+router.post("/vynil/:id/delete", vinyl_controller.vinyl_delete_post); 
+router.get("/vinyl/:id/update", vinyl_controller.vinyl_update_get); 
+router.post("/vinyl/:id/update", vinyl_controller.vinyl_update_post); 
+router.get("/vinyl/:id", vinyl_controller.vinyl_detail);
+router.get("/vinyl", vinyl_controller.vinyl_list);
+
+//vinyinstance routes
+router.get("/copy/create", vinylInstance_controller.copy_create_get);
+router.post("/copy/create", vinylInstance_controller.copy_create_post);
+router.get("/copy/:id/delete", vinylInstance_controller.copy_delete_get);
+router.post("/copy/:id/delete", vinylInstance_controller.copy_delete_post);
+router.get("/copy/:id/update", vinylInstance_controller.copy_update_get);
+router.post("/copy/:id/update", vinylInstance_controller.copy_update_post);
+router.get("/copy/:id", vinylInstance_controller.copy_detail);
+router.get("/copy", vinyl_controller.copy_list);
+
+//genre routes
+router.get("/genre/create", genre_controller.genre_create_get);
+router.post("/genre/create", genre_controller.genre_create_post);
+router.get("/genre/:id/delete", genre_controller.genre_delete_get);
+router.post("/genre/:id/delete", genre_controller.genre_delete_post);
+router.get("/genre/:id/update", genre_controller.genre_update_get);
+router.post("/genre/:id/update", genre_controller.genre_update_post);
+router.get("/genre/:id", genre_controller.genre_detail);
+router.get("/genres", genre_controller.genre_list);
