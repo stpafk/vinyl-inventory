66253cd0b039ed4a6f672c8c8fd32da4842ec78d
stpafk <stephan.weigert@hotmail.com>
2023-11-07 14:34:26 -0300

    add upload route

--- app.js
+++ app.js
@@ -34,6 +34,7 @@ app.use(express.static(path.join(__dirname, 'public')));
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
 app.use('/catalog', catalogRouter);
+app.use('/uploads', express.static("uploads"))
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
