e47cfb979039636f219f3be79a3dd1fbcc7a68bf
stpafk <stephan.weigert@hotmail.com>
2023-11-07 11:14:31 -0300

    add UPDATE + fix typos

--- controllers/artistController.js
+++ controllers/artistController.js
@@ -52,7 +52,7 @@ exports.artist_create_post = [
         });
 
         if (req.file) {
-            artist.image = req.file.path
+            artist.image = req.file.path;
         }
 
         if (!error.isEmpty()) {
@@ -62,10 +62,10 @@ exports.artist_create_post = [
                 errors: error.array(),
             }) 
             return;
-        } else {
-            await artist.save()
-            res.redirect(artist.url)
         }
+        
+        await artist.save();
+        res.redirect(artist.url);
     })
 ]
 
@@ -108,10 +108,47 @@ exports.artist_delete_post = asyncHandler(async (req, res, next) => {
 });
   
 exports.artist_update_get = asyncHandler(async (req, res, next) => {
-    res.render("NOT IMPLEMENTED: artist update GET");
+    const artist = await Artist.findById(req.params.id);
+
+    if (artist === null) {
+        const err = new Error("Artist not found.");
+        err.status = 404;
+        return next(err);
+    }
+
+    res.render("artist_form", {
+        title: "Edit Artist",
+        artist: artist,
+    })
 });
   
-exports.artist_update_post = asyncHandler(async (req, res, next) => {
-    res.render("NOT IMPLEMENTED: artist update POST");
-});
+exports.artist_update_post = [
+    body("artist_name").trim().isLength({min:1}).escape().withMessage("First name must be specified."),
+    body("formed_in").trim().isLength({min:1}).escape().withMessage("Band formation must be specified / Unknown"),
+    body("description").trim().isLength({min: 1}).escape().withMessage("Description must not be empty"),
+    body("image").escape(),
+
+    asyncHandler(async (req, res, next) => {
+
+        const error = validationResult(req);
+        const artist = new Artist({
+            artist_name: req.body.artist_name,
+            formed_in: req.body.formed_in,
+            description: req.body.description,
+            image: req.params.image,
+            _id: req.params.id,
+        });
+
+        if (!error.isEmpty()) {
+            res.render("artist_form", {
+                title: "Edit Artist",
+                artist: artist,
+                errors: error.array(),
+            }) 
+            return;
+        }
+        const updatedArtist = await Artist.findByIdAndUpdate(req.params.id, artist, {});
+        res.redirect(updatedArtist.url);
+    })
+]
 
