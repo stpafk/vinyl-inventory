a17232aa9fe8f417390df88e6633b2f156aebb15
stpafk <stephan.weigert@hotmail.com>
2023-11-08 17:03:42 -0300

    add user route

--- routes/user.js
+++ routes/user.js
@@ -1,6 +1,8 @@
 var express = require('express');
 var router = express.Router();
 
+const loginController = require("../controllers/loginController");
+
 router.get('/user', function(req, res, next) {
   res.send('respond with a resource');
 });
