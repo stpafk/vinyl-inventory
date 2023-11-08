29dff4829fa806b211e53ea56fd457e8eed1081f
stpafk <stephan.weigert@hotmail.com>
2023-11-08 10:39:08 -0300

    add vinyl list and detail

--- controllers/vinylController.js
+++ controllers/vinylController.js
@@ -1,21 +1,38 @@
-const vinyl = require("../models/vinyl");
+const Vinyl = require("../models/vinyl");
+const Genre = require('../models/genre');
+const Artist = require('../models/artist');
+const VinylInstance = require('../models/vinyl_instance');
+const {body, validationResult} = require('express-validator');
 const asyncHandler = require("express-async-handler");
 
 exports.vinyl_list = asyncHandler(async (req, res, next) => {
-    res.send("NOT IMPLEMENTED: vinyl list");
+    const vinyls = await Vinyl.find().sort({vinyl_name: 1}).exec();
+
+    res.render("vinyl_list", {
+        title: "All Vinyls",
+        vinyls: vinyls,
+    });
   });
   
 exports.vinyl_detail = asyncHandler(async (req, res, next) => {
-    res.send(`NOT IMPLEMENTED: vinyl detail: ${req.params.id}`);
+    const [vinyl, copy] = await Promise.all([
+        Vinyl.findById(req.params.id).exec(),
+        VinylInstance.find({vinyl: req.params.id}),
+    ])
+
+    if (vinyl === null) {
+        const err = new Error("Vinyl not found.");
+        err.status = 404;
+        return next(err);
+    }
+
+    res.render("vinyl_detail", {
+        title: "Vinyl Detail",
+        vinyl: vinyl,
+        copy: copy,
+    })
 });
-  
-exports.vinyl_create_get = asyncHandler(async (req, res, next) => {
-res.send("NOT IMPLEMENTED: vinyl create GET");
-});
-  
-exports.vinyl_create_post = asyncHandler(async (req, res, next) => {
-    res.send("NOT IMPLEMENTED: vinyl create POST");
-});
+
   
 exports.vinyl_delete_get = asyncHandler(async (req, res, next) => {
     res.send("NOT IMPLEMENTED: vinyl delete GET");
--- models/vinyl.js
+++ models/vinyl.js
@@ -1,12 +1,13 @@
 const mongoose = require('mongoose');
+const {DateTime} = require('luxon')
 
 const Schema = mongoose.Schema;
 
 const VinylSchema = new Schema({
-    vynil_name: {type: String, required: true},
+    vinyl_name: {type: String, required: true},
     artist: {type: Schema.Types.ObjectId, ref: "Artist", required: true},
     summary: {type: String, required: true},
-    year_of_release: {type: Date, required: true},
+    date_of_release: {type: Date, required: true},
     genre: [{type: Schema.Types.ObjectId, ref: "Genre"}],
     cover: { type: String }
 });
@@ -14,5 +15,9 @@ const VinylSchema = new Schema({
 VinylSchema.virtual("url").get(function() {
     return `/catalog/vynil/${this.id}`;
 });
+
+VinylSchema.virtual("formatted_date").get(function() {
+    return this.date_of_release ? DateTime.fromJSDate(this.date_of_release).toLocaleString(DateTime.MED) : "";
+})
 
 module.exports = mongoose.model("Vinyl", VinylSchema);
