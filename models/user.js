b256576d09237bf0828ba0e72c6022c6bfb1cac5
stpafk <stephan.weigert@hotmail.com>
2023-11-08 17:02:45 -0300

    start user model

---
+++ models/user.js
@@ -0,0 +1,15 @@
+const mongoose = require('mongoose');
+
+const Schema = mongoose.Schema;
+
+const UserSchema = new Schema({
+    username: {type: String, required: true, minLength: 3, maxLength: 100},
+    email: {type: String, required: true, minLength: 3, maxLength: 100},
+    password: {type: String, required: true, minLength: 3, maxLength: 100}, 
+})
+
+UserSchema.virtual("url").get(function() {
+    return `/user/${this.username}`
+});
+
+module.exports = mongoose.Model("User", UserSchema);
