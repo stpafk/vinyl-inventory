9691133b955e920ced254434eeb21b7ddd992c54
stpafk <stephan.weigert@hotmail.com>
2023-11-09 17:32:03 -0300

    add: POST vinyl instance

--- controllers/vinylInstanceController.js
+++ controllers/vinylInstanceController.js
@@ -1,6 +1,7 @@
 const Copy = require("../models/vinyl_instance");
 const asyncHandler = require("express-async-handler");
-const Vinyl = require("../models/vinyl")
+const Vinyl = require("../models/vinyl");
+const {body, validationResult} = require('express-validator');
   
 exports.copy_detail = asyncHandler(async (req, res, next) => {
     const copy = Copy.findById(req.params.id)
@@ -20,12 +21,57 @@ exports.copy_detail = asyncHandler(async (req, res, next) => {
 });
   
 exports.copy_create_get = asyncHandler(async (req, res, next) => {
-res.send("NOT IMPLEMENTED: copy create GET");
+    const vinyl = await Vinyl.find().exec();
+
+    res.render("copy_form", {
+        title: "Create Issue",
+        vinyl: vinyl,
+    })
 });
   
-exports.copy_create_post = asyncHandler(async (req, res, next) => {
-    res.send("NOT IMPLEMENTED: copy create POST");
-});
+exports.copy_create_post = [
+    body("copy_name").trim().isLength({min: 1}).escape("Input vinyl name."),
+    body("year").isLength({min: 0, max: 4}).escape("Input valid year."),
+    body("released_by").trim().isLength({min: 1, max: 100}).escape("Input valid Record Label."),
+    body("released_in").trim().isLength({max: 28}).escape("Input a valid Country."),
+    body("quantity").isInt().escape("Input quantity."),
+    body("price").isInt().escape(),
+    body("status").escape(),
+
+    asyncHandler(async (req, res, next) => {
+
+        const vinylExist = await Vinyl.findOne({vinyl_name: req.body.copy_name})
+        if (vinylExist) {
+            req.body.copy_name = vinylExist._id;
+        }
+        console.log(req.body)
+        const errors = validationResult(req);
+        const copy = new Copy({
+            vinyl: req.body.copy_name,
+            year: req.body.year,
+            released_by: req.body.released_by,
+            released_in: req.body.released_in,
+            quantity: req.body.quantity,
+            price: req.body.price,
+            status: req.body.status,
+        });
+        console.log(errors.array())
+        if (!errors.isEmpty()) {
+            const vinyl = await Vinyl.find().exec();
+            
+            res.render("copy_form", {
+                title: "Create Issue",
+                vinyl: vinyl,
+                copy: copy,
+                errors: errors.array()
+            });
+            return;
+        }
+
+        await copy.save();
+        res.redirect(copy.url)
+    })
+]
   
 exports.copy_delete_get = asyncHandler(async (req, res, next) => {
     res.send("NOT IMPLEMENTED: copy delete GET");
