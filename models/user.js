ad66e61e73cd6c34ccbc3db8c3b521d79bf986af
stpafk <stephan.weigert@hotmail.com>
2023-11-10 16:12:28 -0300

    added: user register form

--- models/user.js
+++ models/user.js
@@ -5,11 +5,11 @@ const Schema = mongoose.Schema;
 const UserSchema = new Schema({
     username: {type: String, required: true, minLength: 3, maxLength: 100},
     email: {type: String, required: true, minLength: 3, maxLength: 100},
-    password: {type: String, required: true, minLength: 3, maxLength: 100}, 
+    password: {type: String, required: true, minLength: 6}, 
 })
 
 UserSchema.virtual("url").get(function() {
     return `/user/${this.username}`
 });
 
-module.exports = mongoose.Model("User", UserSchema);
+module.exports = mongoose.model("User", UserSchema);
